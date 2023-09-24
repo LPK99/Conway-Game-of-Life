383f8c0ac87448ddeef0b79723484be1d2d435f6
Your Name <trungducmic@gmail.com>
2023-09-24 19:57:19 +0700

    updated main.py

--- main.py
+++ main.py
@@ -1,142 +1,65 @@
-# Python code to implement Conway's Game Of Life
-import argparse
+import pygame
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
-# setting up the values for the grid
-ON = 255
-OFF = 0
-vals = [ON, OFF]
 
-def randomGrid(N):
+col_about_to_die = (200, 200, 225)
+col_alive = (255, 255, 215)
+col_background = (10, 10, 40)
+col_grid = (30, 30, 60)
 
-	"""returns a grid of NxN random values"""
-	return np.random.choice(vals, N*N, p=[0.2, 0.8]).reshape(N, N)
 
-def addGlider(i, j, grid):
+def update(surface, cur, sz):
+    nxt = np.zeros((cur.shape[0], cur.shape[1]))
 
-	"""adds a glider with top left cell at (i, j)"""
-	glider = np.array([[0, 0, 255],
-					[255, 0, 255],
-					[0, 255, 255]])
-	grid[i:i+3, j:j+3] = glider
+    for r, c in np.ndindex(cur.shape):
+        num_alive = np.sum(cur[r-1:r+2, c-1:c+2]) - cur[r, c]
 
-def addGosperGliderGun(i, j, grid):
+        if cur[r, c] == 1 and num_alive < 2 or num_alive > 3:
+            col = col_about_to_die
+        elif (cur[r, c] == 1 and 2 <= num_alive <= 3) or (cur[r, c] == 0 and num_alive == 3):
+            nxt[r, c] = 1
+            col = col_alive
 
-	"""adds a Gosper Glider Gun with top left
-	cell at (i, j)"""
-	gun = np.zeros(11*38).reshape(11, 38)
+        col = col if cur[r, c] == 1 else col_background
+        pygame.draw.rect(surface, col, (c*sz, r*sz, sz-1, sz-1))
 
-	gun[5][1] = gun[5][2] = 255
-	gun[6][1] = gun[6][2] = 255
+    return nxt
 
-	gun[3][13] = gun[3][14] = 255
-	gun[4][12] = gun[4][16] = 255
-	gun[5][11] = gun[5][17] = 255
-	gun[6][11] = gun[6][15] = gun[6][17] = gun[6][18] = 255
-	gun[7][11] = gun[7][17] = 255
-	gun[8][12] = gun[8][16] = 255
-	gun[9][13] = gun[9][14] = 255
 
-	gun[1][25] = 255
-	gun[2][23] = gun[2][25] = 255
-	gun[3][21] = gun[3][22] = 255
-	gun[4][21] = gun[4][22] = 255
-	gun[5][21] = gun[5][22] = 255
-	gun[6][23] = gun[6][25] = 255
-	gun[7][25] = 255
+def init(dimx, dimy):
+    cells = np.zeros((dimy, dimx))
 
-	gun[3][35] = gun[3][36] = 255
-	gun[4][35] = gun[4][36] = 255
+    pattern = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0],
+                        [1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                        [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]);
+    pos = (3,3)
+    cells[pos[0]:pos[0]+pattern.shape[0], pos[1]:pos[1]+pattern.shape[1]] = pattern
+    return cells
 
-	grid[i:i+11, j:j+38] = gun
 
-def update(frameNum, img, grid, N):
+def main(dimx, dimy, cellsize):
+    pygame.init()
+    surface = pygame.display.set_mode((dimx * cellsize, dimy * cellsize))
+    pygame.display.set_caption("John Conway's Game of Life")
 
-	# copy grid since we require 8 neighbors
-	# for calculation and we go line by line
-	newGrid = grid.copy()
-	for i in range(N):
-		for j in range(N):
+    cells = init(dimx, dimy)
 
-			# compute 8-neighbor sum
-			# using toroidal boundary conditions - x and y wrap around
-			# so that the simulation takes place on a toroidal surface.
-			total = int((grid[i, (j-1)%N] + grid[i, (j+1)%N] +
-						grid[(i-1)%N, j] + grid[(i+1)%N, j] +
-						grid[(i-1)%N, (j-1)%N] + grid[(i-1)%N, (j+1)%N] +
-						grid[(i+1)%N, (j-1)%N] + grid[(i+1)%N, (j+1)%N])/255)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
 
-			# apply Conway's rules
-			if grid[i, j] == ON:
-				if (total < 2) or (total > 3):
-					newGrid[i, j] = OFF
-			else:
-				if total == 3:
-					newGrid[i, j] = ON
+        surface.fill(col_grid)
+        cells = update(surface, cells, cellsize)
+        pygame.display.update()
 
-	# update data
-	img.set_data(newGrid)
-	grid[:] = newGrid[:]
-	return img,
 
-# main() function
-def main():
-
-	# Command line args are in sys.argv[1], sys.argv[2] ..
-	# sys.argv[0] is the script name itself and can be ignored
-	# parse arguments
-	parser = argparse.ArgumentParser(description="Runs Conway's Game of Life simulation.")
-
-	# add arguments
-	parser.add_argument('--grid-size', dest='N', required=False)
-	parser.add_argument('--mov-file', dest='movfile', required=False)
-	parser.add_argument('--interval', dest='interval', required=False)
-	parser.add_argument('--glider', action='store_true', required=False)
-	parser.add_argument('--gosper', action='store_true', required=False)
-	args = parser.parse_args()
-	
-	# set grid size
-	N = 100
-	if args.N and int(args.N) > 8:
-		N = int(args.N)
-		
-	# set animation update interval
-	updateInterval = 50
-	if args.interval:
-		updateInterval = int(args.interval)
-
-	# declare grid
-	grid = np.array([])
-
-	# check if "glider" demo flag is specified
-	if args.glider:
-		grid = np.zeros(N*N).reshape(N, N)
-		addGlider(1, 1, grid)
-	elif args.gosper:
-		grid = np.zeros(N*N).reshape(N, N)
-		addGosperGliderGun(10, 10, grid)
-
-	else: # populate grid with random on/off -
-			# more off than on
-		grid = randomGrid(N)
-
-	# set up animation
-	fig, ax = plt.subplots()
-	img = ax.imshow(grid, interpolation='nearest')
-	ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
-								frames = 10,
-								interval=updateInterval,
-								save_count=50)
-
-	# # of frames?
-	# set output file
-	if args.movfile:
-		ani.save(args.movfile, fps=30, extra_args=['-vcodec', 'libx264'])
-
-	plt.show()
-
-# call main
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    main(100, 70, 8)
